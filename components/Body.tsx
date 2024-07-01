d7195ce247f3a9534ac4655aebb440dce41f4ffa
Sean McLane <sean@seanmclane.com>
2024-07-01 16:22:50 -0600

    explicit type so build doesn't fail

--- components/Body.tsx
+++ components/Body.tsx
@@ -3,11 +3,18 @@ import { urlForImage } from "@/sanity/sanity.image"
 import { Image } from "next-sanity/image"
 import { getImageDimensions } from "@sanity/asset-utils"
 
+interface Value {
+  value: {
+    asset: any
+    alt: string
+  }
+}
+
 const Body = ({props}: any) => {
 
   const customBlockComponents = {
     types: {
-      image: ({ value }) => {
+      image: ({ value }: Value) => {
         const {width, height} = getImageDimensions(value.asset)
         // we need to get the image source url, and since @sanity/image-url will give us optimised images for each instance we use it
         const imgUrl = urlForImage(value.asset)
