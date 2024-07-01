ecf3019b5b8f41e20fe69e83f82d6995d10b5194
Sean McLane <sean@seanmclane.com>
2024-07-01 16:31:13 -0600

    explicit typing again

--- sanity/sanity.image.ts
+++ sanity/sanity.image.ts
@@ -1,9 +1,10 @@
 import createImageUrlBuilder from "@sanity/image-url"
+import { SanityImageSource } from "@sanity/image-url/lib/types/types"
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || ''
 
 const imageBuilder = createImageUrlBuilder({ projectId, dataset })
 
-export const urlForImage = (source) =>
+export const urlForImage = (source: SanityImageSource) =>
   imageBuilder.image(source).auto("format").fit("max")
