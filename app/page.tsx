c8838d0c68abad43a741872cba82f0e2d462ece5
Sean McLane <sean@seanmclane.com>
2024-07-12 19:25:46 -0600

    missed one

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-center">
-      <ShanLogoHeader />
+      <ShanLogoHeader links={[]}/>
       <PersonaSelector />
     </main>
   )
