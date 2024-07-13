d20eb0bdfb7d1a3140c728dc329b1a2c0bd0e3e5
Sean McLane <sean@seanmclane.com>
2024-07-12 22:50:56 -0600

    one more shadow

--- components/PersonaSelector.tsx
+++ components/PersonaSelector.tsx
@@ -18,7 +18,7 @@ export default async function PersonaSelector({selected}: Props) {
     <div className="flex flex-col items-center px-8 mt-16">
       <h1 className="text-6xl lg:text-8xl">Sean McLane is a 
         <span className="text-theme text-6xl lg:text-8xl font-sans uppercase bg-transparent">
-          <Dropdown defaultState={selected || "_____________"} items={personas} listStyle="bg-white border-theme border-2" />
+          <Dropdown defaultState={selected || "_____________"} items={personas} listStyle="bg-white border-theme border-2 shadow-md" />
         </span>
       </h1>
     </div>
